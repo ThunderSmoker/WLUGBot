14a996dc22fbd195c10b73d5159bdb5c6e477aec
ThunderSmoker <pitlawararadhya@gmail.com>
2023-01-02 13:37:41 +0530

    final one

--- app.py
+++ app.py
@@ -67,13 +67,15 @@ def sms_reply():
     msg=bot_resp.message()
     if 'hello' in user_msg or 'hi' in user_msg or 'hey' in user_msg:
         msg.body("Hello There, I am a Bot created for Walchand Linux users Group 🐧!!\n What can i help you with?")
-    elif 'main board' in user_msg:
-        msg.body("President:Dnyaneshwari Kolapkar\nVice President:Krishnacharan Bhola\n\nfor more visit www.wcewlug.org")
-    elif 'show' in user_msg:
-        msg.body("1.List of Main Board\n2.List of Assistant Board\n3.Register for ongoing event\n4.Linux quote")
-    elif 'logo' in user_msg:
+    elif 'main board' in user_msg or '1' in user_msg:
+        msg.body("President:\nDnyaneshwari Kolapkar\nVice President:\nKrishnacharan Bhola\n\nfor more visit www.wcewlug.org")
+    elif 'show' in user_msg or '6' in user_msg:
+        msg.body("1.List of Main Board\n2.List of Assistant Board\n3.Register for ongoing event\n4.Linux quote\n5.WLUG Logo\n6.Show Commands")
+    elif 'logo' in user_msg or '5' in user_msg:
         msg.media("https://www.wcewlug.org/images/wluglogo.png")
-    elif 'quote' in user_msg:
+    elif 'register' in user_msg or '3' in user_msg:
+        msg.body("No ongoing events from WLUG")
+    elif 'quote' in user_msg or '4' in user_msg:
         r=requests.get('https://api.quotable.io/random')
         if r.status_code==200:
             data=r.json()
@@ -81,6 +83,8 @@ def sms_reply():
         else:
             quote='I could not retrieve a quote, Sorry!'
         msg.body(quote)
+    elif 'assistant board' in user_msg or '2' in user_msg:
+        msg.body("Assistant Technical Advisor:\n Aradhya Pitlawar")
     else: 
         msg.body('Sorry , Didnt get What you said! Send a Valid Command')
     return str(bot_resp) 
